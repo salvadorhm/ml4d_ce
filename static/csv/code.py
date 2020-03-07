fed253602c6bf5bbe8fa24de395e3071d6ade9c7
Salvador HM <salvadorhm@gmail.com>
2020-03-07 09:27:47 -0600

    demo listo

--- static/csv/code.py
+++ static/csv/code.py
@@ -9,20 +9,3 @@ from sklearn.metrics import confusion_matrix
 import matplotlib.mlab as mlab
 # Creando el Dataframe para trabajar
 dataframe = pd.read_csv('Leads.csv')# Describe 'Lead Origin
-dataframe['Lead Origin'].describe()
-# Describe 'Lead Origin
-dataframe['Lead Origin'].describe()
-# Describe 'Lead Origin
-dataframe['Lead Origin'].describe()
-# Describe 'Lead Origin
-dataframe['Lead Origin'].describe()
-# Describe 'Lead Origin
-dataframe['Lead Origin'].describe()
-# Describe 'Lead Origin
-dataframe['Lead Origin'].describe()
-# Describe 'Lead Origin
-dataframe['Lead Origin'].describe()
-# Describe 'Lead Source
-dataframe['Lead Source'].describe()
-# Describe columna 'Lead Origin'
-dataframe['Lead Origin'].describe()
