d47845aea0a796fcc96eec4330ccf6ca9d969ec1
Salvador HM <salvadorhm@gmail.com>
2020-03-25 19:58:27 -0600

    UX actualizado

--- static/code/code.py
+++ static/code/code.py
@@ -16,10 +16,170 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 # Creando el Dataframe para trabajar
-dataframe = pd.read_csv('wine_dataset.csv')
+dataframe = pd.read_csv('demo3x.csv')
 # Descripcion del dataframe
 dataframe.describe()
 # Dataframe
 dataframe
 # Head
 dataframe.head()
+# Preparacion del dataframe
+df_x = dataframe[['x']]
+df_y = dataframe['y']
+# Normalizar dataframe
+df_nor_x = (df_x - df_x.mean())/df_x.std()
+# x
+df_nor_x
+# y
+df_nor_y = (df_y - df_y.mean())/df_y.std()
+df_nor_y
+# Dataframe de entrenamiento y de prueba
+x_train, x_test, y_train, y_test = train_test_split(df_nor_x,df_nor_y,test_size=0.3,random_state=42)
+# Model de regresion lineal
+model = LinearRegression()
+# Entrenamiento del model
+model.fit(x_train,y_train)
+# Prueba del modelo
+predictions = model.predict(x_test)
+# Evaluacion del modelo
+# Coefficients
+model.coef_
+# Independent term
+model.intercept_
+# Mean squared error
+mean_squared_error(y_test, predictions)
+# Mean absolute error
+mean_absolute_error(y_test, predictions)
+# Variance
+r2_score(y_test, predictions)
+# Comparacion de los resultados
+compare = pd.DataFrame({'Actual':y_test, 'Predicted':predictions})
+# Valores de prueba
+compare.Actual.head(10)
+# Valores predichos
+compare.Predicted.head(10)
+# Grafica scatter
+plt.scatter(y_test,predictions)
+# Grafica de distribucion
+sn.distplot(y_test - predictions)
+# Preparacion del dataframe
+df_x = dataframe[['x']]
+df_y = dataframe['y']
+# Normalizar dataframe
+df_nor_x = (df_x - df_x.mean())/df_x.std()
+# x
+df_nor_x
+# y
+df_nor_y = (df_y - df_y.mean())/df_y.std()
+df_nor_y
+# Dataframe de entrenamiento y de prueba
+x_train, x_test, y_train, y_test = train_test_split(df_nor_x,df_nor_y,test_size=0.3,random_state=42)
+# Model de regresion lineal
+model = LinearRegression()
+# Entrenamiento del model
+model.fit(x_train,y_train)
+# Prueba del modelo
+predictions = model.predict(x_test)
+# Evaluacion del modelo
+# Coefficients
+model.coef_
+# Independent term
+model.intercept_
+# Mean squared error
+mean_squared_error(y_test, predictions)
+# Mean absolute error
+mean_absolute_error(y_test, predictions)
+# Variance
+r2_score(y_test, predictions)
+# Comparacion de los resultados
+compare = pd.DataFrame({'Actual':y_test, 'Predicted':predictions})
+# Valores de prueba
+compare.Actual.head(10)
+# Valores predichos
+compare.Predicted.head(10)
+# Grafica scatter
+plt.scatter(y_test,predictions)
+# Grafica de distribucion
+sn.distplot(y_test - predictions)
+# Histogram de x
+sn.distplot(dataframe[x])
+# Histogram de y
+sn.distplot(dataframe[y])
+# Preparacion del dataframe
+df_x = dataframe[['x']]
+df_y = dataframe['y']
+# Normalizar dataframe
+df_nor_x = (df_x - df_x.mean())/df_x.std()
+# x
+df_nor_x
+# y
+df_nor_y = (df_y - df_y.mean())/df_y.std()
+df_nor_y
+# Dataframe de entrenamiento y de prueba
+x_train, x_test, y_train, y_test = train_test_split(df_nor_x,df_nor_y,test_size=0.3,random_state=42)
+# Model de regresion lineal
+model = LinearRegression()
+# Entrenamiento del model
+model.fit(x_train,y_train)
+# Prueba del modelo
+predictions = model.predict(x_test)
+# Evaluacion del modelo
+# Coefficients
+model.coef_
+# Independent term
+model.intercept_
+# Mean squared error
+mean_squared_error(y_test, predictions)
+# Mean absolute error
+mean_absolute_error(y_test, predictions)
+# Variance
+r2_score(y_test, predictions)
+# Comparacion de los resultados
+compare = pd.DataFrame({'Actual':y_test, 'Predicted':predictions})
+# Valores de prueba
+compare.Actual.head(10)
+# Valores predichos
+compare.Predicted.head(10)
+# Grafica scatter
+plt.scatter(y_test,predictions)
+# Grafica de distribucion
+sn.distplot(y_test - predictions)
+# Preparacion del dataframe
+df_x = dataframe[['y']]
+df_y = dataframe['x']
+# Normalizar dataframe
+df_nor_x = (df_x - df_x.mean())/df_x.std()
+# x
+df_nor_x
+# y
+df_nor_y = (df_y - df_y.mean())/df_y.std()
+df_nor_y
+# Dataframe de entrenamiento y de prueba
+x_train, x_test, y_train, y_test = train_test_split(df_nor_x,df_nor_y,test_size=0.3,random_state=42)
+# Model de regresion lineal
+model = LinearRegression()
+# Entrenamiento del model
+model.fit(x_train,y_train)
+# Prueba del modelo
+predictions = model.predict(x_test)
+# Evaluacion del modelo
+# Coefficients
+model.coef_
+# Independent term
+model.intercept_
+# Mean squared error
+mean_squared_error(y_test, predictions)
+# Mean absolute error
+mean_absolute_error(y_test, predictions)
+# Variance
+r2_score(y_test, predictions)
+# Comparacion de los resultados
+compare = pd.DataFrame({'Actual':y_test, 'Predicted':predictions})
+# Valores de prueba
+compare.Actual.head(10)
+# Valores predichos
+compare.Predicted.head(10)
+# Grafica scatter
+plt.scatter(y_test,predictions)
+# Grafica de distribucion
+sn.distplot(y_test - predictions)
--- webdataminingtool.py
+++ webdataminingtool.py
@@ -1,7 +1,7 @@
 import web  # pip install web.py
 import os
 
-app_version = "0.7.3"  # version de la webapp
+app_version = "0.7.4"  # version de la webapp
 
 urls = (
     '/','application.controllers.index.Index',
